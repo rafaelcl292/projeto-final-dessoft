1136ff23ebd627111885fcecf639789e5632ea9a
rafaelcl292 <rafaelcl@al.insper.edu.br>
2021-05-21 00:01:00 -0300

    .

--- classes.py
+++ classes.py
@@ -4,20 +4,118 @@ import pygame
 class Background():
     def __init__(self, window):
         self.window = window
+        self.fase = 0
+        self.posicao = 0
+        self.velocidade = 0
         self.grama = pygame.transform.scale(pygame.image.load('tiles/grama.png'), (50, 50))
+        fase1 = [
+            [0]*100,  # y = 0
+            [0]*100,  # y = 50
+            [0]*100,  # y = 100
+            [0]*100,  # y = 150
+            [0]*100,  # y = 200
+            [0]*100,  # y = 250
+            [0]*100,  # y = 300
+            [1]*100,  # y = 350
+            [0]*100,  # y = 400
+            [0]*100,  # y = 450
+            [0]*100,  # y = 500
+            [0]*100,  # y = 550
+            [0]*100,  # y = 600
+            [1]*5 + [0] + [1]*94,  # y = 650
+        ]
+        fase2 = [
+            [0]*100,  # y = 0
+            [0]*100,  # y = 50
+            [0]*100,  # y = 100
+            [0]*100,  # y = 150
+            [0]*100,  # y = 200
+            [0]*100,  # y = 250
+            [0]*100,  # y = 300
+            [0]*100,  # y = 350
+            [0]*100,  # y = 400
+            [0]*100,  # y = 450
+            [0]*100,  # y = 500
+            [0]*100,  # y = 550
+            [0]*100,  # y = 600
+            [0]*100,  # y = 650
+        ]
+        fase3 = [
+            [0]*100,  # y = 0
+            [0]*100,  # y = 50
+            [0]*100,  # y = 100
+            [0]*100,  # y = 150
+            [0]*100,  # y = 200
+            [0]*100,  # y = 250
+            [0]*100,  # y = 300
+            [0]*100,  # y = 350
+            [0]*100,  # y = 400
+            [0]*100,  # y = 450
+            [0]*100,  # y = 500
+            [0]*100,  # y = 550
+            [0]*100,  # y = 600
+            [0]*100,  # y = 650
+        ]
+        fase4 = [
+            [0]*100,  # y = 0
+            [0]*100,  # y = 50
+            [0]*100,  # y = 100
+            [0]*100,  # y = 150
+            [0]*100,  # y = 200
+            [0]*100,  # y = 250
+            [0]*100,  # y = 300
+            [0]*100,  # y = 350
+            [0]*100,  # y = 400
+            [0]*100,  # y = 450
+            [0]*100,  # y = 500
+            [0]*100,  # y = 550
+            [0]*100,  # y = 600
+            [0]*100,  # y = 650
+        ]
+        fase5 = [
+            [0]*100,  # y = 0
+            [0]*100,  # y = 50
+            [0]*100,  # y = 100
+            [0]*100,  # y = 150
+            [0]*100,  # y = 200
+            [0]*100,  # y = 250
+            [0]*100,  # y = 300
+            [0]*100,  # y = 350
+            [0]*100,  # y = 400
+            [0]*100,  # y = 450
+            [0]*100,  # y = 500
+            [0]*100,  # y = 550
+            [0]*100,  # y = 600
+            [0]*100,  # y = 650
+        ]
+        self.fases = [fase1, fase2, fase3, fase4, fase5]
+        self.block_address_index = [
+            [None, self.grama],  # fase 1
+            [None],  # fase 2
+            [None],  # fase 3
+            [None],  # fase 4
+            [None],  # fase 5
+        ]
     
 
     def load(self):
+        self.posicao += self.velocidade
         self.window.fill((69,179,224))
-        for x in range(0, 1200, 50):
-            self.window.blit(self.grama, (x, 650))
+        for y, linha in enumerate(self.fases[self.fase]):
+            for x, block_address in enumerate(linha):
+                block = self.block_address_index[self.fase][block_address]
+                if block_address != 0:
+                    self.window.blit(block, (x*50 + self.posicao, y*50))
 
 
 class Personagem():
     def __init__(self, window):
         self.window = window
         self.personagem = pygame.transform.scale(pygame.image.load('personagem_temporario.png'), (50, 100))
+        self.posicao = 550
+        self.pulando = False
+        self.contador_pulo = 25
     
 
     def load(self):
-        self.window.blit(self.personagem, (200, 550))
+        self.window.blit(self.personagem, (200, self.posicao))
--- main.py
+++ main.py
@@ -5,19 +5,45 @@ pygame.init()
 w = 1200
 h = 700
 window = pygame.display.set_mode((w, h))
-game = True
+clock = pygame.time.Clock()
 
+background = classes.Background(window)
+personagem = classes.Personagem(window)
+
+game = True
 while game:
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             game = False
-        # Background
-        background = classes.Background(window)
-        background.load()
-        # Personagem
-        personagem = classes.Personagem(window)
-        personagem.load()
-        # Update
-        pygame.display.update()
+        # Movimentos no background
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                background.velocidade += 8
+            elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                background.velocidade -= 8
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                background.velocidade -= 8
+            elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                background.velocidade += 8
+        # Movimentos do personagem
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE or event.key == pygame.K_UP or event.key == pygame.K_w:
+                personagem.pulando = True
+    if personagem.pulando:
+        if personagem.contador_pulo >= -25:
+            personagem.posicao -= personagem.contador_pulo ** 2 * 0.05 * -(int(personagem.contador_pulo < 0) * 2 - 1)
+            personagem.contador_pulo -= 1
+        else:
+            personagem.pulando = False
+            personagem.contador_pulo = 25
+    # Background
+    background.load()
+    # Personagem
+    personagem.load()
+    # Update
+    pygame.display.update()
+    # Clock tick
+    clock.tick(60)
 
 pygame.quit()
