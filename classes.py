cbddded4f74e5a89b5b71270dfbc02d571c44523
rafaelcl292 <rafaelcl@al.insper.edu.br>
2021-06-07 22:45:27 -0300

    .

--- classes.py
+++ classes.py
@@ -194,8 +194,8 @@ class Personagem():
     def __init__(self, window):
         self.window = window
         self.altura = 100
-        self.largura = 50
-        self.personagem = pygame.transform.scale(pygame.image.load('sprites_player/player_parado.png'), (self.largura*2, self.altura))
+        self.largura = 40
+        self.personagem = pygame.transform.scale(pygame.image.load('sprites_player/player_parado.png'), (self.largura + 60, self.altura))
         self.posicao_y = 500
         self.posicao_x = 200
         self.velocidade_x = 0
